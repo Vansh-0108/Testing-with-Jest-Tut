788a46a75fa40781efed2091851a2de6ef311092
Vansh-0108 <vanshmahajan2001@gmail.com>
2024-02-07 11:24:51 +0530

    Password Checker Iter-1

--- src/app/pass_checker/PassCheck.ts
+++ src/app/pass_checker/PassCheck.ts
@@ -1,5 +1,11 @@
 export class PassCheck {
-    public checkPassword(){
-        
+    public checkPassword(pass: string):  boolean{
+        if(pass.length<8)
+            return false;
+        if(pass.toLowerCase() == pass)
+            return false;
+        if(pass.toUpperCase() == pass)
+            return false;
+        return true;
     }
 }
--- src/test/pass_checker/PassCheck.test.ts
+++ src/test/pass_checker/PassCheck.test.ts
@@ -8,7 +8,33 @@ describe('Password Checker Tests Suite', () => {
         sut = new PassCheck();
     })
     
-    it('should do nothing', () => {
-        sut.checkPassword();
+    it('should mark the pass with less than 8 chars as invalid', () => {
+        const actual = sut.checkPassword('abcdEFG');
+        expect(actual).toBe(false);
+    })
+
+    it('should mark the pass with 8 or more chars as valid', () => {
+        const actual = sut.checkPassword('abcdEFGH');
+        expect(actual).toBe(true);
+    })
+
+    it('should mark the pass with no lowercase char as invalid', () => {
+        const actual = sut.checkPassword('ABCDEFGH');
+        expect(actual).toBe(false);
+    })
+
+    it('should mark the pass with lowercase char as valid', () => {
+        const actual = sut.checkPassword('abcdEFGH');
+        expect(actual).toBe(true);
+    })
+
+    it('should mark the pass with no uppercase char as invalid', () => {
+        const actual = sut.checkPassword('abcdefgh');
+        expect(actual).toBe(false);
+    })
+
+    it('should mark the pass with uppercase char as valid', () => {
+        const actual = sut.checkPassword('abcdEFGH');
+        expect(actual).toBe(true);
     })
 })
