a3cbcae042a0426cfca4e0f39fb884235a8af90c
Vansh-0108 <vanshmahajan2001@gmail.com>
2024-02-12 17:07:01 +0530

    Changes in IdGenerator.test.ts

--- src/test/server_app/data/IdGenerator.test.ts
+++ src/test/server_app/data/IdGenerator.test.ts
@@ -7,8 +7,9 @@ jest.mock('crypto', () => ({
 describe('generateRandomId test Suite', () => {
     
     test("should generate a random number", () => {
-       // jest.spyOn(global.crypto, randomBytes).mockReturnValue(0)
+        // jest.spyOn(global.crypto, randomBytes).mockReturnValue(0)
         const actual = generateRandomId();
-        expect(actual).toBe('865f38a9950699794e81fcd91584f8612f5a42aec5b7bbed48c1683832c519c22c836c91fe1afc0330a2ea02dea0a31a1f509dfde1a780ce82ec0eb1');
+        expect(actual.length).toBe(120);
+        // expect(actual).toBe('865f38a9950699794e81fcd91584f8612f5a42aec5b7bbed48c1683832c519c22c836c91fe1afc0330a2ea02dea0a31a1f509dfde1a780ce82ec0eb1');
     })
 })
